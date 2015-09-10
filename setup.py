07e2a15e7b25df93bd48e5c084016ced6cfacd9b
Simó Albert i Beltran <sim6@probeta.net>
2015-09-10 17:35:12 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='primestg',
+    version='0.1.0',
+    packages=find_packages(),
+    url='https://github.com/gisce/primestg',
+    license='GNU Affero General Public License v3',
+    author='GISCE-TI, S.L.',
+    author_email='devel@gisce.net',
+    description='Prime STG-DC Interface Specification'
+)
