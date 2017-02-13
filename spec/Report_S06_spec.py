9b360cef1dd912225a659295942df3250bb1a75f
MiquelIR <misernroca@gisce.net>
2017-02-13 11:54:09 +0100

    Mod S06 tests to work with various files
    and added empty report file

--- spec/Report_S06_spec.py
+++ spec/Report_S06_spec.py
@@ -6,19 +6,35 @@ from ast import literal_eval
 with description('Report S06 example'):
     with before.all:
 
-        self.data_filename = 'spec/data/S06.xml'
+        self.data_filenames = [
+            'spec/data/S06.xml',
+            # 'spec/data/S06_empty.xml'
+        ]
 
-        with open(self.data_filename) as data_file:
-            self.report = Report(data_file)
+        self.report = []
+        for data_filename in self.data_filenames:
+            with open(data_filename) as data_file:
+                self.report.append(Report(data_file))
 
     with it('generates the expected results for the whole report'):
 
-        result_filename = '{}_result.txt'.format(self.data_filename)
+        result_filenames = []
+        for data_filename in self.data_filenames:
+            result_filenames.append('{}_result.txt'.format(data_filename))
 
-        with open(result_filename) as result_file:
-            result_string = result_file.read()
-            self.expected_result = literal_eval(result_string)
+        for key, result_filename in enumerate(result_filenames):
+            with open(result_filename) as result_file:
+                result_string = result_file.read()
+                expected_result = literal_eval(result_string)
 
-        result = self.report.values
-
-        expect(result).to(equal(self.expected_result))
+        result = self.report[key].values
+        expect(result).to(equal(expected_result))
+        # result_filename = '{}_result.txt'.format(self.data_filename)
+        #
+        # with open(result_filename) as result_file:
+        #     result_string = result_file.read()
+        #     self.expected_result = literal_eval(result_string)
+        #
+        # result = self.report.values
+        #
+        # expect(result).to(equal(self.expected_result))
