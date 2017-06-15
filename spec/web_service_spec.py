ae253d24bbd015c3f022511e59a3b0534983df68
MiquelIR <misernroca@gisce.net>
2017-06-15 17:23:29 +0200

    Test-Add test for S02 S04 and S05

--- spec/web_service_spec.py
+++ spec/web_service_spec.py
@@ -10,15 +10,48 @@ with description('Web services run'):
     with before.all:
         self.s = Service()
 
-    with it('asking for S02 report'):
+    with it('asking for S02 report with mocked connection'):
         with responses.RequestsMock() as rsps:
             rsps.add(responses.POST, 'http://cct.gisce.lan:8080/',
-                     body='{"error": "expected error"}', status=404)
+                     body='{"error": "expected S02 error"}', status=404)
             try:
                 resp = self.s.get_daily_incremental('ZIV0040318130',
                                                     '20170610010000',
                                                     '20170611000000')
 
             except TransportError as te:
-                print te
-                assert 'expected error' in te.message
+                assert 'expected S02 error' in te.message
+
+    with it('asking for S02 report for all meters with mocked connection'):
+        with responses.RequestsMock() as rsps:
+            rsps.add(responses.POST, 'http://cct.gisce.lan:8080/',
+                     body='{"error": "expected S02 error"}', status=404)
+            try:
+                resp = self.s.get_all_daily_incremental('20170610010000',
+                                                        '20170611000000')
+
+            except TransportError as te:
+                assert 'expected S02 error' in te.message
+
+    with it('asking for S04 report with mocked connection'):
+        with responses.RequestsMock() as rsps:
+            rsps.add(responses.POST, 'http://cct.gisce.lan:8080/',
+                     body='{"error": "expected S04 error"}', status=404)
+            try:
+                resp = self.s.get_monthly_billing('ZIV0040318130',
+                                                  '20170600010000',
+                                                  '20170631000000')
+
+            except TransportError as te:
+                assert 'expected S04 error' in te.message
+
+    with it('asking for S05 report with mocked connection'):
+        with responses.RequestsMock() as rsps:
+            rsps.add(responses.POST, 'http://cct.gisce.lan:8080/',
+                     body='{"error": "expected S05 error"}', status=404)
+            try:
+                resp = self.s.get_all_daily_absolute('20170609010000',
+                                                     '20170611000000')
+
+            except TransportError as te:
+                assert 'expected S05 error' in te.message
