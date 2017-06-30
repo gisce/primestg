7fc58c651ea4d2d9db8b37a9f9768cecc834b225
MiquelIR <misernroca@gisce.net>
2017-06-30 12:47:24 +0200

    Add function to get relative path in repository

--- primestg/__init__.py
+++ primestg/__init__.py
@@ -1,5 +1,14 @@
+import os
+
 try:
     __version__ = __import__('pkg_resources') \
         .get_distribution(__name__).version
 except Exception, e:
     __version__ = 'unknown'
+
+_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_data(path):
+    filename = isinstance(path, (list, tuple)) and path[0] or path
+    return os.path.join(_ROOT, 'data', filename)
