5daa75521006102346022abbe14f81b179264348
Eduard Carreras <ecarreras@gisce.net>
2019-06-04 15:55:52 +0200

    Bump to v1.15.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='primestg',
-    version='1.14.0',
+    version='1.15.0',
     packages=find_packages(),
     url='https://github.com/gisce/primestg',
     license='GNU Affero General Public License v3',
