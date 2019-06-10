b4122f97e45c7673e9fe975c0898d38e3cb73a58
scampos <scampos@gisce.net>
2019-06-10 13:42:02 +0200

    b11 using libcomxml

--- primestg/service.py
+++ primestg/service.py
@@ -1,19 +1,9 @@
 # -*- coding: UTF-8 -*-
-
 from zeep import Client
-import primestg
 from datetime import datetime
+import primestg
+from order.orders import *
 import calendar
-
-
-B11_TEMPLATE = """<Order IdPet="{idpet}" IdReq="B11" Version="3.1.c">
-    <Cnc Id="{cnc_id}">
-        <B11 Order="{order}" Args="" Fini="{start_date}" Ffin="{end_date}">
-        </B11>
-    </Cnc>
-</Order>
-"""
-
 
 def last_sunday(year, month):
     """Retorna l'últim diumenge del mes, serveix per determinar quin dia
@@ -61,36 +51,27 @@ class Service(object):
             results = self.DC_service.AsynchRequest(self.fact_id, report_id,
                                                     date_from, date_to,
                                                     meters, 2, self.source)
-
         return results
 
-    def order(self, cnc_id, order, start_date=None, end_date=None):
-        """
-        :param cnc_id: Id concentrador
-        :type cnc_id: str
-        :param order: Tipo de order (T01, T02, T03, T04, ...)
-        :type order: str
-        :param start_date: Fecha inicio, si está vacio coge ahora)
-        :type start_date: datetime
-        :param end_date: Fecha final, si está vacio coge ahora)
-        :type end_date: datetime
-        :return: Resultado petición
-        :rtype: bool
-        """
+    def send_order(self, report_id, order):
+        results = self.DC_service.Order(self.fact_id, 0, order, 3)
+        return results
 
-        if start_date is None:
-            start_date = datetime.now()
-        if end_date is None:
-            end_date = datetime.now()
-
-        b11 = B11_TEMPLATE.format(
-            idpet=self.fact_id,
-            cnc_id=cnc_id,
-            order=order,
-            start_date=format_timestamp(start_date),
-            end_date=format_timestamp(end_date)
-        )
-        return self.DC_service.Order(self.fact_id, 0, b11, 3)
+    def get_order_request(self, generic_values, payload):
+        """
+        Sends B11 order to concentrator
+        :return: Success or fail
+        """
+        payload.update({
+            'date_from': format_timestamp(payload.get('date_from')),
+            'date_to': format_timestamp(payload.get('date_to'))
+        })
+        b11 = B11(generic_values, payload)
+        b11.order.build_tree()
+        b11.order.pretty_print = True
+        b11 = str(b11.order)
+        formatted_b11 = b11.replace('<?xml version=\'1.0\' encoding=\'UTF-8\'?>\n', '')
+        return self.send_order('B11', formatted_b11)
 
     def create_service(self):
         binding = '{http://www.asais.fr/ns/Saturne/DC/ws}WS_DCSoap'
@@ -105,6 +86,29 @@ class Service(object):
         :return: an S01 report for the corresponding meter
         """
         return self.send('S01', meters)
+
+    def get_advanced_instant_data(self, meters):
+        """
+        Asks for a S21 report to the specified meter.
+        :param meters: a meter_id
+        :return: an S21 report for the corresponding meter
+        """
+        return self.send('S21', meters)
+
+    def get_contract_definition(self, meters, date_from, date_to):
+        """
+        Asks for a S23 report to the specified meter.
+        :param meters: a meter_id
+        :return: an S23 report for the corresponding meter
+        """
+        return self.send('S23', meters, date_from, date_to)
+
+    def get_all_contract_definition(self, date_from, date_to):
+        """
+        Asks for a S23 report to all meters.
+        :return: an S23 report from every meter
+        """
+        return self.send('S23', '', date_from, date_to)
 
     def get_daily_incremental(self, meters, date_from, date_to):
         """
@@ -151,21 +155,6 @@ class Service(object):
         """
         return self.send('S05', '', date_from, date_to)
 
-    def get_meter_parameters(self, meters, date_from, date_to):
-        """
-        Asks for a S06 report to the specified meter.
-        :param meters: a meter_id
-        :return: an S06 report for the corresponding meter
-        """
-        return self.send('S06', meters, date_from, date_to)
-
-    def get_all_meter_parameters(self, date_from, date_to):
-        """
-        Asks for a S06 report to all meters.
-        :return: an S06 report from every meter
-        """
-        return self.send('S06', '', date_from, date_to)
-
     def get_meter_events(self, meters, date_from, date_to):
         """
         Asks for a S09 report to the specified meter.
@@ -181,6 +170,21 @@ class Service(object):
         """
         return self.send('S09', '', date_from, date_to)
 
+    def get_meter_parameters(self, meters, date_from, date_to):
+        """
+        Asks for a S06 report to the specified meter.
+        :param meters: a meter_id
+        :return: an S06 report for the corresponding meter
+        """
+        return self.send('S06', meters, date_from, date_to)
+
+    def get_all_meter_parameters(self, date_from, date_to):
+        """
+        Asks for a S06 report to all meters.
+        :return: an S06 report from every meter
+        """
+        return self.send('S06', '', date_from, date_to)
+
     def get_concentrator_parameters(self, dc, date_from, date_to):
         """
         Asks for a S12 report to the concentrator.
@@ -188,28 +192,12 @@ class Service(object):
         """
         return self.send('S12', dc, date_from, date_to)
 
-    def get_advanced_instant_data(self, meters):
+    def get_concentrator_meters(self, dc, date_from, date_to):
         """
-        Asks for a S21 report to the specified meter.
-        :param meters: a meter_id
-        :return: an S21 report for the corresponding meter
+        Asks for a S17 report to the concentrator.
+        :return: an S17 report from the concentrator.
         """
-        return self.send('S21', meters)
-
-    def get_contract_definition(self, meters, date_from, date_to):
-        """
-        Asks for a S23 report to the specified meter.
-        :param meters: a meter_id
-        :return: an S23 report for the corresponding meter
-        """
-        return self.send('S23', meters, date_from, date_to)
-
-    def get_all_contract_definition(self, date_from, date_to):
-        """
-        Asks for a S23 report to all meters.
-        :return: an S23 report from every meter
-        """
-        return self.send('S23', '', date_from, date_to)
+        return self.send('S12', dc, date_from, date_to)
 
     def get_all_contract_definition(self, date_from, date_to):
         """
