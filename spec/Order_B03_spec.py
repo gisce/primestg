4e7c4c819de01d33a571a82ee8de34a234c1e24d
scampos <scampos@gisce.net>
2019-06-18 11:28:35 +0200

    B03,B09 tests

---
+++ spec/Order_B03_spec.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from primestg.order.orders import Order
+from expects import expect, equal
+
+with description('Order B03 Generation'):
+
+    with it('generates expected B03 xml'):
+        expected_result = '<Order IdPet="1234" IdReq="B03" Version="3.1.c">\n  ' \
+                          '<Cnc Id="CIR000000000">\n    <Cnt Id="CNT000000000">\n      ' \
+                          '<B03 Ffin="" Fini="" Order="5"/>\n    </Cnt>\n  </Cnc>\n</Order>\n'
+
+        generic_values = {
+            'id_pet': str(1234),
+            'id_req': 'B03',
+            'cnc': 'CIR000000000',
+            'cnt': 'CNT000000000'
+        }
+        payload = {
+            'order_param': '5',
+            'date_from': '',
+            'date_to': '',
+        }
+        order = Order('B03')
+        order = order.create(generic_values, payload)
+        expect(order).to(equal(expected_result))
