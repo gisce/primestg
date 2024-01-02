651a5433d0bbfcbfca494dd9855acec79555894b
tinogis <afita@gisce.net>
2024-01-02 17:08:17 +0100

    Bump to v1.43.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='primestg',
-    version='1.42.0',
+    version='1.43.0',
     packages=find_packages(),
     url='https://github.com/gisce/primestg',
     license='GNU Affero General Public License v3',
