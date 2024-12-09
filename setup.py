abe29cfb5aad60979a0e6fa7a0083386a4e92cad
Release <github-actions@github.com>
2024-12-09 12:39:48 +0100

    Bump to v1.53.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name='primestg',
-    version='1.52.1',
+    version='1.53.0',
     packages=find_packages(),
     url='https://github.com/gisce/primestg',
     license='GNU Affero General Public License v3',
