00aa71281847ae3be0ac45180cde65a6a905cb3d
PSala <polsalalidon@gmail.com>
2025-07-01 18:06:05 +0200

    Bump to v1.56.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name='primestg',
-    version='1.56.4',
+    version='1.56.5',
     packages=find_packages(),
     url='https://github.com/gisce/primestg',
     license='GNU Affero General Public License v3',
