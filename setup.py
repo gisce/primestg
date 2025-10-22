24bc085723f2928792cc6879d9ec5f774326c324
Agustí Fita Borrell <afita@gisce.net>
2025-10-22 17:30:58 +0200

    Bump to v1.58.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name='primestg',
-    version='1.57.0',
+    version='1.58.0',
     packages=find_packages(),
     url='https://github.com/gisce/primestg',
     license='GNU Affero General Public License v3',
