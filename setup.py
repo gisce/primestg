60f39d2cedb6ffccb1def0f48eeb7c060d843aa7
Release <github-actions@github.com>
2025-10-30 12:08:27 +0100

    Bump to v1.57.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name='primestg',
-    version='1.57.0',
+    version='1.57.1',
     packages=find_packages(),
     url='https://github.com/gisce/primestg',
     license='GNU Affero General Public License v3',
